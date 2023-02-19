105465a6c5db163cc246608e6d6f92cffcf335b8
Lorenz Tesch <121463323+LorenzTesch@users.noreply.github.com>
2023-02-19 18:49:45 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ When one tries to access the darknet "Incognito Market", they might be presented
 
 ![Example of an ANONGUARD captcha](https://github.com/LorenzTesch/Captcha-Solver-Incognito-Market-ANONGUARD/blob/main/example/solved.png?raw=true)
 
-This script is able to provide the correct solution to it automatically.
-It must be executed on the page, when the captcha is visible.
+This script is able to provide the correct solution to it automatically.\
+It must be executed on the page, when the captcha is visible.\
+\
+Canvases must be enabled. Tor blocks them by default. Go to 'about:config', and set privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts to false.\
+The challenges might need multiple solvings.
--- solver.js
+++ solver.js
@@ -1,125 +1,128 @@
-/* This value might need some tweaking. The pixels of the 2 images, do not share the exact color values. When they are compared later on, we must account for some varience. */
-const allowedVariance = 50;
+(()=>{
 
-const inpSlider = document.querySelector('input[type=range]');
+    /* This value might need some tweaking. The pixels of the 2 images, do not share the exact color values. When they are compared later on, we must account for some varience. */
+    const allowedVariance = 50;
 
-const sliderMin = parseInt(inpSlider.min);
-const sliderMax = parseInt(inpSlider.max);
-const offsetTop = parseInt(window.getComputedStyle(inpSlider).getPropertyValue('top'));
+    const inpSlider = document.querySelector('input[type=range]');
 
-
-const imgCaptcha = new Image();
-const imgSlider = new Image();
-
-var resolveCaptcha = undefined;
-var resolveSlider = undefined;
-
-var waitForLoad = Promise.all([
-    new Promise((_res) => { resolveCaptcha = _res; }),
-    new Promise((_res) => { resolveSlider = _res; })
-]);
-
-waitForLoad.then(solve);
-
-imgCaptcha.onload = resolveCaptcha;
-imgSlider.onload = resolveSlider;
-
-/* Obtains the base64 code of the images. */
-const captchaBase64 = window.getComputedStyle(document.querySelector('.captcha')).backgroundImage.replace('url("', '').replace('")', '');
-const sliderBase64 = document.head.querySelector('style').innerHTML.split(/::-moz-range-thumb[ {\n]+background-image:[ ]?url\(/)[1].split(')')[0];
-
-imgCaptcha.src = captchaBase64;
-imgSlider.src = sliderBase64;
-
-function isSimiliar(x, y){
-    return Math.abs(y - x) <= allowedVariance;
-}
-
-function solve(){
-
-    console.log('Both images loaded.');
-
-    var canvasCaptcha = document.createElement('canvas');
-    canvasCaptcha.width = imgCaptcha.width;
-    canvasCaptcha.height = imgCaptcha.height;
-
-    var contextCaptcha = canvasCaptcha.getContext('2d');
-    contextCaptcha.drawImage(imgCaptcha, 0, 0);
-
-    var imageDataCaptcha = contextCaptcha.getImageData(0, 0, canvasCaptcha.width, canvasCaptcha.height);
+    const sliderMin = parseInt(inpSlider.min);
+    const sliderMax = parseInt(inpSlider.max);
+    const offsetTop = parseInt(window.getComputedStyle(inpSlider).getPropertyValue('top'));
 
 
-    var canvasSlider = document.createElement('canvas');
-    canvasSlider.width = imgSlider.width;
-    canvasSlider.height = imgSlider.height;
+    const imgCaptcha = new Image();
+    const imgSlider = new Image();
 
-    var contextSlider = canvasSlider.getContext('2d');
-    contextSlider.drawImage(imgSlider, 0, 0);
+    var resolveCaptcha = undefined;
+    var resolveSlider = undefined;
 
-    var imageDataSlider = contextSlider.getImageData(0, 0, canvasSlider.width, canvasSlider.height);
+    var waitForLoad = Promise.all([
+        new Promise((_res) => { resolveCaptcha = _res; }),
+        new Promise((_res) => { resolveSlider = _res; })
+    ]);
 
+    waitForLoad.then(solve);
 
-    /* crop image to relevant line */
-    imageDataCaptcha = [...imageDataCaptcha.data].slice(
-        canvasCaptcha.width * offsetTop * 4,
-        canvasCaptcha.width * (offsetTop + 1/* sliderHeight */) * 4,
-    );
+    imgCaptcha.onload = resolveCaptcha;
+    imgSlider.onload = resolveSlider;
 
-    imageDataSlider = [...imageDataSlider.data].slice(
-        0,
-        imgSlider.width * 4
-    );
+    /* Obtains the base64 code of the images. */
+    const captchaBase64 = window.getComputedStyle(document.querySelector('.captcha')).backgroundImage.replace('url("', '').replace('")', '');
+    const sliderBase64 = document.head.querySelector('style').innerHTML.split(/::-moz-range-thumb[ {\n]+background-image:[ ]?url\(/)[1].split(')')[0];
 
-    var imageDataCaptchaReduced = [];
-    var imageDataSliderReduced = [];
+    imgCaptcha.src = captchaBase64;
+    imgSlider.src = sliderBase64;
 
-    for(let i = 0; i < imageDataCaptcha.length; i+=4){
-        //imageDataCaptchaReduced.push(`${imageDataCaptcha[i + 0]},${imageDataCaptcha[i + 1]},${imageDataCaptcha[i + 2]},${imageDataCaptcha[i + 3]}`);
-        imageDataCaptchaReduced.push(imageDataCaptcha[i + 0] + imageDataCaptcha[i + 1] + imageDataCaptcha[i + 2]); // sum up the rgb values
+    function isSimiliar(x, y) {
+        return Math.abs(y - x) <= allowedVariance;
     }
 
-    for (let i = 0; i < imageDataSlider.length; i += 4) {
-        //imageDataSliderReduced.push(`${imageDataSlider[i + 0]},${imageDataSlider[i + 1]},${imageDataSlider[i + 2]},${imageDataSlider[i + 3]}`);
-        imageDataSliderReduced.push(imageDataSlider[i + 0] + imageDataSlider[i + 1] + imageDataSlider[i + 2]); // sum up the rgb values
-    }
+    function solve() {
 
-    console.log(imageDataCaptchaReduced);
-    console.log(imageDataSliderReduced);
+        console.log('Both images loaded.');
 
-    var maxStreak = 0;
-    var maxStreakOffset = 0;
+        var canvasCaptcha = document.createElement('canvas');
+        canvasCaptcha.width = imgCaptcha.width;
+        canvasCaptcha.height = imgCaptcha.height;
 
-    /* Find the best match of the pixel's color values. */
-    for (let i = 0; i < imageDataCaptchaReduced.length - imageDataSliderReduced.length; i++){
+        var contextCaptcha = canvasCaptcha.getContext('2d');
+        contextCaptcha.drawImage(imgCaptcha, 0, 0);
 
-        if (isSimiliar(imageDataCaptchaReduced[i], imageDataSliderReduced[0])){
+        var imageDataCaptcha = contextCaptcha.getImageData(0, 0, canvasCaptcha.width, canvasCaptcha.height);
 
-            var rightOffset = i;
 
-            for (let j = 0; j < imageDataSliderReduced.length; j++){
+        var canvasSlider = document.createElement('canvas');
+        canvasSlider.width = imgSlider.width;
+        canvasSlider.height = imgSlider.height;
 
-                if (!isSimiliar(imageDataCaptchaReduced[rightOffset + j], imageDataSliderReduced[j])){
-                    break;
-                }
+        var contextSlider = canvasSlider.getContext('2d');
+        contextSlider.drawImage(imgSlider, 0, 0);
 
-                if (j > maxStreak){
-                    maxStreak = j;
-                    maxStreakOffset = i;
-                    console.log('streak at', i, j);
+        var imageDataSlider = contextSlider.getImageData(0, 0, canvasSlider.width, canvasSlider.height);
+
+
+        /* crop image to relevant line */
+        imageDataCaptcha = [...imageDataCaptcha.data].slice(
+            canvasCaptcha.width * offsetTop * 4,
+            canvasCaptcha.width * (offsetTop + 1/* sliderHeight */) * 4,
+        );
+
+        imageDataSlider = [...imageDataSlider.data].slice(
+            0,
+            imgSlider.width * 4
+        );
+
+        var imageDataCaptchaReduced = [];
+        var imageDataSliderReduced = [];
+
+        for (let i = 0; i < imageDataCaptcha.length; i += 4) {
+            //imageDataCaptchaReduced.push(`${imageDataCaptcha[i + 0]},${imageDataCaptcha[i + 1]},${imageDataCaptcha[i + 2]},${imageDataCaptcha[i + 3]}`);
+            imageDataCaptchaReduced.push(imageDataCaptcha[i + 0] + imageDataCaptcha[i + 1] + imageDataCaptcha[i + 2]); // sum up the rgb values
+        }
+
+        for (let i = 0; i < imageDataSlider.length; i += 4) {
+            //imageDataSliderReduced.push(`${imageDataSlider[i + 0]},${imageDataSlider[i + 1]},${imageDataSlider[i + 2]},${imageDataSlider[i + 3]}`);
+            imageDataSliderReduced.push(imageDataSlider[i + 0] + imageDataSlider[i + 1] + imageDataSlider[i + 2]); // sum up the rgb values
+        }
+
+        console.log(imageDataCaptchaReduced);
+        console.log(imageDataSliderReduced);
+
+        var maxStreak = 0;
+        var maxStreakOffset = 0;
+
+        /* Find the best match of the pixel's color values. */
+        for (let i = 0; i < imageDataCaptchaReduced.length - imageDataSliderReduced.length; i++) {
+
+            if (isSimiliar(imageDataCaptchaReduced[i], imageDataSliderReduced[0])) {
+
+                var rightOffset = i;
+
+                for (let j = 0; j < imageDataSliderReduced.length; j++) {
+
+                    if (!isSimiliar(imageDataCaptchaReduced[rightOffset + j], imageDataSliderReduced[j])) {
+                        break;
+                    }
+
+                    if (j > maxStreak) {
+                        maxStreak = j;
+                        maxStreakOffset = i;
+                        console.log('streak at', i, j);
+                    }
+
                 }
 
             }
 
         }
 
+        /* Calculate how much the slider should move. */
+        var answer = (sliderMax - sliderMin) * ((maxStreakOffset + imgSlider.width / 2) / imgCaptcha.width) + sliderMin;
+
+        inpSlider.value = answer;
+
+        console.log(sliderMin, sliderMax, maxStreak, maxStreakOffset);
+        console.log('The answer is', answer);
     }
 
-    /* Calculate how much the slider should move. */
-    var answer = (sliderMax - sliderMin) * ( ( maxStreakOffset + imgSlider.width/2 ) / imgCaptcha.width) + sliderMin;
-
-    inpSlider.value = answer;
-
-    console.log('The answer is', answer);
-}
-
-
+})()
